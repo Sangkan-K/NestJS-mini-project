df827f5b3dfe7de5364ee2b7181378043bf305ab
Sangkan-K <gimsanghan53@gmail.com>
2022-08-26 17:25:11 +0900

    Nest 기본 정리중

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,8 @@
 // root module
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { BoardsModule } from './boards/boards.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [BoardsModule],
 })
 export class AppModule {}
