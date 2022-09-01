5cd366c17250d8370d21b441f852ec18123739e0
Sangkan-K <gimsanghan53@gmail.com>
2022-09-01 19:35:41 +0900

    uuid

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
-import { Board } from './board.model';
+import { Board, BoardStatus } from './board.model';
+import { v1 as uuid } from 'uuid';
 
 @Injectable()
 export class BoardsService {
@@ -8,4 +9,18 @@ export class BoardsService {
   getAllBoards(): Board[] {
     return this.boards;
   }
+
+  createBoard(title: string, description: string) {
+    const board: Board = {
+      id: uuid(),
+      title,
+      description,
+      status: BoardStatus.PUBLIC,
+    };
+
+    this.boards.push(board);
+    return board;
+  }
 }
+// Board에 ID가 있었기 떄문에 오류가 발생한다. 로컬 메모리를 사용할 때는 임의의 유니크한 값을 부여해야한다.
+// 여기서는 uuid 모듈을 활용해보자. (npm install uuid --save)
