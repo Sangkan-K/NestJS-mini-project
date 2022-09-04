bac67062577558aa7c46b844e09a3d94d9622f88
Sangkan-K <gimsanghan53@gmail.com>
2022-09-04 23:02:12 +0900

    a

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { title } from 'process';
 import { Board } from './board.model';
 import { BoardsService } from './boards.service';
 
@@ -10,4 +11,14 @@ export class BoardsController {
   getAllBoard(): Board[] {
     return this.boardsService.getAllBoards();
   }
+
+  @Post()
+  createBoard(
+    @Body('title') title: string,
+    @Body('description') description: string,
+  ): Board {
+    return this.boardsService.createBoard(title, description);
+  }
 }
+// express에서는 클라이언트의 요청을 req.body로 받고
+// nest JS에서는 @Body() body로 받아온다.
